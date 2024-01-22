4d9852b85ddda73137e500855af0104952e9d49e
Murenzi Paterne <murenzi419@gmail.com>
2024-01-22 15:16:26 +0200

    blog page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const animated_cursor = document.querySelector(".animated_cursor");
-
+// animating the mouse
 window.addEventListener("mousemove", (e) => {
+  const animated_cursor = document.querySelector(".animated_cursor");
   animated_cursor.style.left = e.pageX + "px";
   animated_cursor.style.top = e.pageY + "px";
 });
