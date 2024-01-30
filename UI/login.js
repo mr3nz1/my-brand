755b56212f8accc85173ec30f3d2046592e92d63
Murenzi Paterne <murenzi419@gmail.com>
2024-01-30 14:46:28 +0200

    built basic login boilerplate

---
+++ UI/login.js
@@ -0,0 +1,30 @@
+const userEmailInput = document.getElementById("user_email_input");
+const userPasswordInput = document.getElementById("user_password_input");
+const loginForm = document.getElementById("login_form");
+const loginFormErrorElement = document.getElementById("form_error_text")
+
+// login
+async function checkCredentials(email, password) {
+  const user = await JSON.parse(localStorage.getItem("user"));
+
+  if (email === user.email && password === user.password) {
+    return true
+  }
+
+  return false
+}
+
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const userEmail = userEmailInput.value
+  const userPassword = userPasswordInput.value
+
+  const credentialsMatch = await checkCredentials(userEmail, userPassword)
+
+  if (credentialsMatch) {
+
+  } else {
+    loginFormErrorElement.style.display = "block"
+    loginFormErrorElement.innerHTML = "Error logging in. Incorrect email or password"
+  }  
+});
