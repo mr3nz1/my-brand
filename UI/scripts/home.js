a88283686a15368aa8082962bf491504781e89b6
Murenzi Paterne <murenzi419@gmail.com>
2024-02-02 11:26:22 +0200

    added unpublished articles to the dashboard home page

--- UI/scripts/home.js
+++ UI/scripts/home.js
@@ -1,67 +1,16 @@
 import { loadPage } from "../dashboard/script.js";
 
-const currentPage = localStorage.getItem("currentPage");
-
-function deleteArticle(articleId) {
-  if (
-    confirm("Are you sure you want to delete article with ID: " + articleId)
-  ) {
-    const articles = JSON.parse(localStorage.getItem("articles"));
-    let newArticles = articles.filter((article) => article.id !== articleId);
-
-    localStorage.setItem("articles", newArticles);
-    location.reload();
-  }
-}
-
-window.addEventListener("blogLoaded", (e) => {
-  const articles = JSON.parse(localStorage.getItem("articles"));
-
-  const publishedArticles = articles.filter(
-    (article) => article.published === true
-  );
-  const unPublishedArticles = articles.filter(
-    (article) => article.published !== true
-  );
-
-  const publishedArticlesContainer = document.getElementById(
-    "published_articles_container"
-  );
+window.addEventListener("homeLoaded", () => {
   const unPublishedArticlesContainer = document.getElementById(
     "unpublished_articles_container"
   );
 
-  let publishedArticlesContent = "";
+  const articles = JSON.parse(localStorage.getItem("articles"));
+  const unPublishedArticles = articles.filter(
+    (article) => article.published !== true
+  );
+
   let unPublishedArticlesContent = "";
-
-  publishedArticles.forEach((article) => {
-    publishedArticlesContent += `
-    <div class="article_container">
-    <div class="article_info_container">
-          <h3 class=""><a class="underline_on_hover" href="./article.html">${article.title}</a>
-          </h3>
-          <p>
-              ${article.description}
-          </p>
-          <div class="article_meta_data">
-              <p>${article.created_at}</p>
-              <span class="dot_separator"></span>
-              <p>14 min read</p>
-              <span class="dot_separator"></span>
-              <div class="likes_description">
-                  <img src="../assets/icons/heart.png" alt="" />
-                  <p>83 likes</p>
-              </div>
-              <span class="dot_separator"></span>
-              <span class="underline_on_hover article_update_btn" id="${article.id}">Update</span>
-              <span class="dot_separator"></span>
-              <span class="underline_on_hover article_delete_btn" id="${article.id}">Delete</span>
-          </div>
-      </div>
-      <img class="article_img" src="${article.image}" alt="" />
-      </div>`;
-  });
-
   unPublishedArticles.forEach((article) => {
     unPublishedArticlesContent += `
     <div class="article_container">
@@ -89,14 +38,10 @@ window.addEventListener("blogLoaded", (e) => {
       <img class="article_img" src="${article.image}" alt="" />
       </div>`;
   });
-
-  publishedArticlesContainer.innerHTML = publishedArticlesContent;
   unPublishedArticlesContainer.innerHTML = unPublishedArticlesContent;
 
-  // window.addEventListener("loadedPage", () => {
   const deleteBtns = document.querySelectorAll(".article_delete_btn");
   const updateBtns = document.querySelectorAll(".article_update_btn");
-  // const articles = JSON.parse(localStorage.getItem("articles"));
 
   // listen to delete btns
   deleteBtns.forEach((deleteBtn) => {
@@ -123,5 +68,3 @@ window.addEventListener("blogLoaded", (e) => {
     });
   });
 });
-
-export { deleteArticle };
