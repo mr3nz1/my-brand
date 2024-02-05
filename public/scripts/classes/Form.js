3b4c0bee087df00927d443aa588f1bb91be69844
Murenzi Paterne <murenzi419@gmail.com>
2024-02-05 11:29:15 +0200

    template boilerplate

---
+++ public/scripts/classes/Form.js
@@ -0,0 +1,12 @@
+export {};
+// export class Form implements Inputs {
+//   private inputs: Inputs;
+//   [key: string]: HTMLInputElement;
+//   constructor(
+//     submitBtn: HTMLButtonElement,
+//     inputs: Inputs,
+//     customTextContent: HTMLDivElement | null
+//   ) {
+//     this.inputs = inputs;
+//   }
+// }
